2cef7e3acd84b58bba8718293f7a849c61efaa46
Amka <Aminhassen1470@gmail.com>
2023-07-29 20:42:01 -0400

    update home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,33 @@
-<!-- `prose` class is comming from `@tailwindcss/typography` plugin -->
-<!-- see more info about `@tailwindcss/typography` plugin (https://tailwindcss.com/docs/typography-plugin) -->
-<div class="prose">
-	<h1>Welcome to TTPLAYS Stack</h1>
-	<p>Technologies ready for building a fullstack web application</p>
-</div>
+<section class="bg-gray-900">
+	<div class="prose h-screen overflow-auto p-4 pt-10">
+		<h1 class="text-white">TTPLAYS Demo MongoDB</h1>
+		<p class="text-gray-400">
+			A Demo application using Sveltekit TTPLAYS Stack and MongoDB as a Database
+		</p>
+
+		<!-- pages -->
+		<h2 class="text-gray-400">Available Pages</h2>
+		<ul>
+			<li>
+				<a href="/profile" class="text-blue-500">Profile</a>
+				<ul class="text-gray-400">
+					<li>You can access this page when logged in</li>
+				</ul>
+			</li>
+			<li>
+				<a href="/login" class="text-blue-500">Login</a>
+
+				<ul class="text-gray-400">
+					<li>You can access this page when not logged in</li>
+				</ul>
+			</li>
+			<li>
+				<a href="/register" class="text-blue-500">Register</a>
+
+				<ul class="text-gray-400">
+					<li>You can access this page when not logged in</li>
+				</ul>
+			</li>
+		</ul>
+	</div>
+</section>
